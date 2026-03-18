bf3cba9a1e54466891ff69b0ca6df163966ce329
Bruce Li <isanoob1897@outlook.com>
2026-03-18 16:42:45 -0700

    add

---
+++ eigenTest2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using namespace std;
+
+int main(void) {
+    MatrixXd m = MatrixXd::Random(3,3);
+
+}
